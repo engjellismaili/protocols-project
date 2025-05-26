969ba693416483138ef77afd898bae9a5ef5e90c
engjellismaili <engjell.ismaili@epfl.ch>
2025-05-26 13:58:13 +0200

    added a new contract

---
+++ proto3/contracts/ThirdProtocol.sol
@@ -0,0 +1,4 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;   // latest stable (May 2025)
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
